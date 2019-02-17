b8702bbd51d565ab0e2e0ba7c7a9233217ae1a2c
yanhongfei123 <970733805@qq.com>
2019-02-17 19:28:14 +0800

    Update userInfo.vue

--- app-rxx-m/pages/user/userInfo/userInfo.vue
+++ app-rxx-m/pages/user/userInfo/userInfo.vue
@@ -152,32 +152,7 @@
 				isWordCardUploaded: false,
 				isshowbox: false,
 				typeId: [],
-				items: [{
-                    value: 'USA',
-                    name: '美国'
-                },
-                {
-                    value: 'CHN',
-                    name: '中国',
-                    checked: 'true'
-                },
-                {
-                    value: 'BRA',
-                    name: '巴西'
-                },
-                {
-                    value: 'JPN',
-                    name: '日本'
-                },
-                {
-                    value: 'ENG',
-                    name: '英国'
-                },
-                {
-                    value: 'FRA',
-                    name: '法国'
-                }
-            ]
+				
 			}
 		},
 		methods: {
